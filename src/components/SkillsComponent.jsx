e47207cf806e6079ffc3ee6c76f20456e45d068a
miasilvia <135966912+miasilvia@users.noreply.github.com>
2024-03-28 14:24:43 +0700

    update

--- src/components/SkillsComponent.jsx
+++ src/components/SkillsComponent.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from "react";
-import { useSwipeable } from "react-swipeable";
 export default function SkillsComponent() {
   const [current, setCurrent] = useState(0);
   const images = [
-    "https://th.bing.com/th/id/OIP.pqcPskVdTrJqfhZ-Z49AtQHaHn?rs=1&pid=ImgDetMain",
-    "https://th.bing.com/th/id/OIP.fBJ2R5Y0m_tQXUxdc0icPQHaKd?rs=1&pid=ImgDetMain",
-    "https://logodownload.org/wp-content/uploads/2022/04/javascript-logo-0.png",
-    "https://th.bing.com/th/id/OIP.K-4RqDC6zFrpAG31ayDDOgHaHa?rs=1&pid=ImgDetMain",
-    "https://th.bing.com/th/id/OIP.S-SYtYzIhgPRnmRd8yWH4gAAAA?rs=1&pid=ImgDetMain",
-    "https://th.bing.com/th/id/OIP.udRsxeiTDWCEXRaKP_prTAHaHk?rs=1&pid=ImgDetMain",
+    "https://logodownload.org/wp-content/uploads/2016/10/html5-logo.png",
+    "https://logodownload.org/wp-content/uploads/2017/04/css-3-logo.png",
+    "https://th.bing.com/th/id/OIP.DN7ToydkJZEdVaJVK_NhvwAAAA?rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0",
+    "https://seeklogo.com/images/T/tailwind-css-logo-89E99D7181-seeklogo.com.png",
+    "https://logospng.org/download/bootstrap/bootstrap-4096.png",
+    "https://iconape.com/wp-content/png_logo_vector/node-js-2.png",
   ];
 
-  const handlers = useSwipeable({
-    onSwipedLeft: () =>
-      setCurrent(current === images.length - 1 ? 0 : current + 1),
-    onSwipedRight: () =>
-      setCurrent(current === 0 ? images.length - 1 : current - 1),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
   return (
     <>
       <h1 className="text-xl font-bold"> Knowledges</h1>
@@ -48,38 +40,38 @@ export default function SkillsComponent() {
           <div>Web Design</div>
           <div>Administration</div>
           <div>Content Creator</div>
-          <div>Growth Maindset</div>
-          <div>Management Skill</div>
           <div>Content Creator</div>
         </div>
       </div>
 
-      <div className="flex justify-center items-center h-screen" {...handlers}>
+      <div className="flex justify-center items-center mt-32  rounded-lg">
         <button
-          className="p-4 bg-blue-500 text-white"
+          className="p-4 bg-[#000000] text-white"
           onClick={() =>
             setCurrent(current === 0 ? images.length - 1 : current - 1)
           }
         >
-          Prev
+          <i className="fas fa-arrow-left"></i>
         </button>
-        <div className="flex justify-center items-center space-x-4">
+        <div className="flex justify-center items-center space-x-4 w-full">
           {images.slice(current, current + 3).map((image, index) => (
             <img
               key={index}
               src={image}
               alt=""
-              className="w-1/4 h-auto transition-transform duration-500 ease-in-out transform-gpu"
+              className={`xl:w-32 xl:h-32 lg:w-24 lg:h-24 md:w-24 m:h-24 w-12 h-12 transition-transform duration-500 ease-in-out transform-gpu ${
+                current === index ? "scale-100" : "scale-90"
+              }`}
             />
           ))}
         </div>
         <button
-          className="p-4 bg-blue-500 text-white"
+          className="p-4 bg-[#000000] text-white"
           onClick={() =>
             setCurrent(current === images.length - 1 ? 0 : current + 1)
           }
         >
-          Next
+          <i className="fas fa-arrow-right"></i>
         </button>
       </div>
     </>
